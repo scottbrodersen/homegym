f5b873dd23b53a9869c0ae106514b11b04ef431d
Scott Brodersen <scott.d.brodersen@gmail.com>
2023-11-12 07:47:51 -0500

    add build modes
    dev mode enables the Vue browser plugin and no minification

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,40 +2,47 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/homegym/home/',
-  mode: 'development',
-  plugins: [
-    vue({ template: { transformAssetUrls } }),
-
-    quasar({
-      sassVariables: 'src/quasar-variables.sass',
-      extras: ['material-icons'],
-    }),
-  ],
-  css: {
-    modules: {
-      localsConvention: 'dashesOnly',
+export default defineConfig(({ command, mode }) => {
+  const config = {
+    base: '/homegym/home/',
+    plugins: [
+      vue({ template: { transformAssetUrls } }),
+      quasar({
+        sassVariables: 'src/quasar-variables.sass',
+        extras: ['material-icons'],
+      }),
+    ],
+    css: {
+      modules: {
+        localsConvention: 'dashesOnly',
+      },
     },
-  },
-  build: {
-    outDir: '../server/secured/dist',
-    manifest: 'homegym-manifest.json',
-    sourcemap: true,
-    minify: false,
-    //    watch: {},
-    mode: 'development',
-    emptyOutDir: true,
-    rollupOptions: {
+    build: {
+      outDir: '../server/secured/dist',
+      manifest: 'homegym-manifest.json',
+      emptyOutDir: true,
+    },
+  };
+
+  if (mode == 'dev') {
+    config.mode = 'development';
+    config.build.mode = 'development';
+    config.build.watch = {};
+    config.build.minify = false;
+    config.build.sourcemap = true;
+    config.build.rollupOptions = {
       output: {
+        compact: false,
         entryFileNames: `assets/entry-[name].js`,
         chunkFileNames: `assets/chunk-[name].js`,
         assetFileNames: `assets/asset-[name].[ext]`,
-        compact: false,
-        preserveModules: false,
       },
-      preserveEntrySignatures: true,
-    },
-  },
+    };
+  } else if (mode == 'prod') {
+    config.mode = 'production';
+    config.build.minify = 'esbuild';
+    config.optimizeDeps = { force: true };
+  }
+
+  return config;
 });
